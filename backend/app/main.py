67ea5afe162f9f0193b29d945057430eb9bce8bd
Eddie Ayala <heriberto.ayala1981@gmail.com>
2025-05-03 20:56:38 -0400

    small changes were made in mian.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,9 +11,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=[
+        "http://localhost:3000",
+        "http://10.0.0.50:3000"
+    ],
     allow_credentials=True,
-    allow_methods=["*"],  
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
